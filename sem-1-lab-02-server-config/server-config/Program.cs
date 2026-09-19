eb2b7173788cc9e68b79667aa51da910da94f9c3
Никита Шеметов <shemetovnikita2008@gmail.com>
2026-09-19 21:55:27 +0500

    add Tests

---
+++ sem-1-lab-02-server-config/server-config/Program.cs
@@ -0,0 +1,18 @@
+﻿namespace MainProject;
+
+public class Program
+{
+    public static int Add(int a, int b)
+    {
+        return a + b;
+    }
+
+    public static bool IsEven(int number)
+    {
+        return number % 2 == 0;
+    }
+
+    public static void Main()
+    {
+    }
+}
---
+++ sem-1-lab-02-server-config/server-configTests/UnitTest1.cs
@@ -0,0 +1,14 @@
+﻿using MainProject;
+
+namespace ProgramTests;
+
+public class Tests
+{
+    [Test]
+    public void Add_TwoNumbers_ReturnsSum()
+    {
+        var result = Program.Add(2, 3);
+
+        Assert.That(result, Is.EqualTo(5));
+    }
+}
