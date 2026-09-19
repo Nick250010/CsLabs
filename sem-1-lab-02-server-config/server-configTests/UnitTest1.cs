e54d74b633c0945dbbc502c854df5c5ad662de71
Никита Шеметов <shemetovnikita2008@gmail.com>
2026-09-19 21:59:46 +0500

    3 tests add passed

--- sem-1-lab-02-server-config/server-configTests/UnitTest1.cs
+++ sem-1-lab-02-server-config/server-configTests/UnitTest1.cs
@@ -11,4 +11,20 @@ public class Tests
 
         Assert.That(result, Is.EqualTo(5));
     }
+
+    [Test]
+    public void IsEven_EvenNumber_ReturnsTrue()
+    {
+        var result = Program.IsEven(10);
+
+        Assert.That(result, Is.True);
+    }
+
+    [Test]
+    public void IsEven_OddNumber_ReturnsFalse()
+    {
+        var result = Program.IsEven(11);
+
+        Assert.That(result, Is.False);
+    }
 }
